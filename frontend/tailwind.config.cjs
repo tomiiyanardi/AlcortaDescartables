c803f7a0d8fcb8e406e238ff313dfe8666172b86
tomiiyanardi <tomasyanardi@gmail.com>
2025-11-17 12:01:34 -0300

    FIX: Simplificar rutas 'content' en tailwind.config.cjs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,13 +1,8 @@
-// 1. Importamos el módulo 'path' de Node.js
-const path = require('path');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // 2. Usamos path.join para crear rutas absolutas
-  // __dirname es una variable de Node que significa "esta carpeta actual"
   content: [
-    path.join(__dirname, "./index.html"),
-    path.join(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
